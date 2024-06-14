cf6a1f5462f93f5db8d1a48ed0f985365bab007c
Lucas Kenji <lucasknishida@gmail.com>
2024-06-14 17:45:08 -0300

    first commit

---
+++ CaixaRegistradora/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace CaixaRegistradora
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Quantidade}x ----- {Nome} ----- {(Preco * Quantidade).ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ CaixaRegistradora/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace CaixaRegistradora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite a quantidade de produtos: ");
+            int quantidadeTotal = int.Parse(Console.ReadLine());
+
+            decimal valorTotal = 0;
+
+            Produto[] produtos = new Produto[quantidadeTotal];
+
+            for(int i = 1; i <= quantidadeTotal; i++)
+            {
+                Console.Write("Produto: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Preço: ");
+                decimal preco = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Quantidade: ");
+                int quantidade = int.Parse(Console.ReadLine());
+
+                produtos[i - 1] = new Produto { Nome = nome, Preco = preco, Quantidade = quantidade };
+
+                valorTotal += preco * quantidade;
+
+                Console.WriteLine("------------------------------------------");
+            }
+
+            Console.Clear();
+            Console.WriteLine("CHECKOUT");
+
+            foreach ( Produto produto in produtos )
+            {
+                Console.WriteLine(produto);
+            }
+
+            Console.WriteLine($"Valor total: $ {valorTotal}");
+        }
+    }
+}
